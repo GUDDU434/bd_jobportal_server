9e811f561df67c0cdb711dfe48d54ed47592c484
GUDDU ALI <gudduali93@yahoo.com>
2024-12-12 21:36:26 +0530

    Updated simple test to check build trigger

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 // Check if the server is running
 app.get("/start", (req, res) => {
-  res.send("Welcome to BD JOB PORTAL SERVER v1.0.0");
+  res.send("Welcome to BD JOB PORTAL SERVER v1.0.0 testing");
 });
 
 // Job Portal API routes
